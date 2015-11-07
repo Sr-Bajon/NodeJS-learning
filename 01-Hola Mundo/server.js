7edd114187fe68657110fd8641a2062e1ce8994a
IsmaelR <ismael.dai@mail.com>
2015-11-07 08:40:34 +0100

    first commit

---
+++ 01-Hola Mundo/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('Hola Mundo!');
+});
+
+
+app.listen(3000, function(){
+  console.log('Servidor iniciado en http://localhost:3000');
+  console.log('Presiona Ctrl + C para terminar');
+});
