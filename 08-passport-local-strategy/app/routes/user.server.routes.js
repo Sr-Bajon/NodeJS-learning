81c05fe0114fb74937ffef5beba0c4ed1d9dcc0c
Ismael-Rodriguez <ismael.dai@gmail.com>
2015-12-03 12:12:42 +0100

    correcciones menores

--- 08-passport-local-strategy/app/routes/user.server.routes.js
+++ 08-passport-local-strategy/app/routes/user.server.routes.js
@@ -1,48 +1,48 @@
 var userController = require('../controllers/user.server.controller'),
-    passport = require('passport');
+    passport       = require('passport');
 
 var util = require('util');
 
-module.exports = function(app){
-    app.get('/registro', function(req, res){
-        res.status(200);
-        res.type('text/html');
-        res.render('registro');
-    });
+module.exports = function (app) {
+  app.get('/registro', function (req, res) {
+    res.status(200);
+    res.type('text/html');
+    res.render('registro');
+  });
 
-    app.post('/crear', userController.crear);
+  app.post('/crear', userController.crear);
 
-    app.route('/login')
-        .get(function(req, res) {
-            res.status(200);
-            res.type('text/html');
-            res.render('login', {
-                messageError: req.flash('error'),
-                messageSuccess: req.flash('success')
-            });
-        })
-        .post(passport.authenticate('local',
-            {
-                successRedirect: '/login',
-                failureRedirect: '/login',
-                failureFlash: true,
-                successFlash: true
-            }
-        ));
+  app.route('/login')
+    .get(function (req, res) {
+      res.status(200);
+      res.type('text/html');
+      res.render('login', {
+        messageError  : req.flash('error'),
+        messageSuccess: req.flash('success')
+      });
+    })
+    .post(passport.authenticate('local',
+      {
+        successRedirect: '/login',
+        failureRedirect: '/login',
+        failureFlash   : true,
+        successFlash   : true
+      }
+    ));
 
-    app.get('/logout', function(req, res){
-        req.logout();
-        res.redirect('/');
+  app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
 
-    });
+  });
 
-    app.get('/navegar', userController.autorizado,
-        function(req, res){
-            res.status(200);
-            res.type('text/html');
-            res.render('navegar', {
-                mensaje: req.flash('mensaje'),
-                usuario: req.user
-            })
+  app.get('/navegar', userController.autorizado,
+    function (req, res) {
+      res.status(200);
+      res.type('text/html');
+      res.render('navegar', {
+        mensaje: req.flash('mensaje'),
+        usuario: req.user
+      })
     });
 };
--- 08-passport-local-strategy/config/passport.config.js
+++ 08-passport-local-strategy/config/passport.config.js
@@ -1,5 +1,5 @@
 var passport = require('passport'),
-    User = require('mongoose').model('User');
+    User     = require('mongoose').model('User');
 
 /*
 *   Normalmente las credenciales solo se obtienen durante el login, si la
@@ -11,22 +11,27 @@ var passport = require('passport'),
 *   req.user
 * */
 
-module.exports = function() {
-    //passport-session
-    passport.serializeUser(function (user, done) {
-        done(null, user.id);
+module.exports = function () {
+  //passport-session
+  // en este ejemplo, solo se serializa el id del usuario, manteniendo los datos de sesion pequeños
+  // las peticiones subsecuentes recividas usaran este ID para encontrar al usuario
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    // en esta funcion debemos buscar el dato serializado en serializeUser en la base de
+    // datos, en este caso se usa mongoose, pero puede hacerse de cualquier otra forma
+
+    //findById acepta como segundo argumento un objeto para indicar que campos
+    //se recuperan, no nos interesa recuperar el password
+    User.findById(id, {password: 0}, function (err, user) {
+      done(err, user);
     });
+  });
 
-    passport.deserializeUser(function (id, done) {
-        //findById acepta como segundo argumento un objeto para indicar que campos
-        //se recuperan, no nos interesa recuperar el password
-        User.findById(id, {password: 0}, function (err, user) {
-            done(err, user);
-        });
-    });
+  //requerimos las estrategias
+  require('./strategies/local.strategy.config')();
 
-    //requerimos las estrategias
-    require('./strategies/local.strategy.config')();
-
-    return passport;
+  return passport;
 };
